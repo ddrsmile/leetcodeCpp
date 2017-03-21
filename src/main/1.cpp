c8e227dbdaad31a0938ebc15b02b9b6c4bb6274c
Liu <nantsou.liu@rakuten.com>
2017-03-21 14:55:36 +0900

    added parser's template

--- src/main/1.cpp
+++ src/main/1.cpp
@@ -2,17 +2,19 @@
 #include <vector>
 #include <string>
 #include "sols.cpp"
-#include "utils/InputHandler.cpp"
+#include "utils/inputparser/IntegerParser.cpp"
 #include "utils/utils.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
     string path = GetFilePath(argc, argv);
-    InputHandler ih(path);
+    IntegerParser integerParser = IntegerParser(path);
+    //InputHandler ih(path);
     Solution sol = Solution();
 
-    vector<vector<int>> inIntVector = ih.GetDataAsIntVector();
+    //vector<vector<int>> inIntVector = ih.GetDataAsIntVector();
+    vector<vector<int>> inIntVector = integerParser.parseDataAsList();
     for (int i = 0; i < inIntVector.size()/2; i++) {
         vector<int> nums = inIntVector[2*i];
         int target = inIntVector[2*i + 1][0];
